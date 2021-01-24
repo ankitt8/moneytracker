7a2e7744f3ede527a64d7e5ce062be52297c43c8
Ankit Tiwari <ankit90499@gmail.com>
2021-01-24 23:13:12 +0530

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -14,11 +14,9 @@ import ReduxThunk from 'redux-thunk';
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 

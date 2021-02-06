e7fbc10c2d40d2ec411238188bbad346ac149579
Ankit Tiwari <ankit90499@gmail.com>
2021-02-06 18:46:36 +0530

    Code Splitting using React.lazy based on route - Used React Lazy method to dynamically import the UpcomingFeature compoonent - The component will be loaded only when its first time rendered - Wrapped the switch in suspense and provided a fallback UI of loader till the module is imported - Verified the same by seeing the sources tab and a new chunk is created having only code of UpcomingFeature

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import './App.css';
 import SnackBarFeedback from './components/SnackBarFeedback';
-import Transactions from './components/Transactions';
+import Transactions from "./components/Transactions";
 import TransactionSummary from './components/TransactionSummary';
-import QuoteAndAddIcon from './components/QuoteAndAddIcon'
+import QuoteAndAddIcon from './components/QuoteAndAddIcon';
 import Header from './components/Header';
-// import Fab from '@material-ui/core/Fab';
-// import AddIcon from '@material-ui/icons/Add';
-import UpcomingFeature from './components/UpcomingFeature';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Loader from "./components/Loader";
+const UpcomingFeature = lazy(() => import('./components/UpcomingFeature'));
 
 function App() {
   return (
     <Router>
       <Header />
-      <Switch >
-        
-        <Route path="/bankaccounts">
-          <UpcomingFeature />
-        </Route>
-        <Route path="/investments">
-          <UpcomingFeature />
-        </Route>
-        <Route path="/budget">
-          <UpcomingFeature />
-        </Route>
-        <Route path="/analysis">
-          <UpcomingFeature />
-        </Route>
-        <Route path="/food-tracker">
-          <UpcomingFeature />
-        </Route>
-        <Route path="/">
-          <div style={{ marginTop: '60px' }}>
-            <TransactionSummary />
-            <QuoteAndAddIcon />
-            <Transactions />
-            <SnackBarFeedback />
-          </div>
-        </Route>
-      </Switch>
+      <Suspense fallback={<Loader />}>
+        <Switch >
+          <Route path="/bankaccounts">
+            <UpcomingFeature />
+          </Route>
+          <Route path="/investments">
+            <UpcomingFeature />
+          </Route>
+          <Route path="/budget">
+            <UpcomingFeature />
+          </Route>
+          <Route path="/analysis">
+            <UpcomingFeature />
+          </Route>
+          <Route path="/food-tracker">
+            <UpcomingFeature />
+          </Route>
+          <Route path="/">
+            <>
+              <TransactionSummary />
+              <QuoteAndAddIcon />
+              <Transactions />
+              <SnackBarFeedback />
+            </>
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }

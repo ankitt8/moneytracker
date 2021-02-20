14dea2167b774086dc806ec0e61f5b270f25169d
Ankit Tiwari <ankit90499@gmail.com>
2021-02-20 15:09:33 +0530

    Moved AddTransaction and Quote logic to seperate components - Used typescript to create the component - Added typescript type for functions it makes the code more easily - understandable and readable

---
+++ src/components/Quote.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quote: React.FC = () => {
+    return (
+        <div className="quote">You should know where you are spending</div>
+    )
+}
+
+export default Quote;

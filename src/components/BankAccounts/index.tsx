14d9594378b71142db2af59503a1c4152f041e6f
Ankit Tiwari <ankit90499@gmail.com>
2021-03-13 21:38:14 +0530

    feature-bank-accounts-page
    - First Commit

---
+++ src/components/BankAccounts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const BankAccount = () => {
+  return (
+    <div className={styles.bankAccounts}>
+      BankAccounts
+    </div>
+  )
+}
+
+export default BankAccount;

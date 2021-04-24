8dae55cd57d16a230f52f5f5b5c30cca0be809a8
Ankit Tiwari <ankit90499@gmail.com>
2021-04-24 18:20:55 +0530

    issue-36-performance-improvement-using-react-memo
    - used react.memo to do reconciliation only when props change

--- src/components/TransactionCardWrapper/index.tsx
+++ src/components/TransactionCardWrapper/index.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import TransactionCard from 'components/TransactionCard';
 import { TransactionCardWrapperProps } from './interface';
 import styles from './styles.module.scss';
-import { Transaction } from 'interfaces/index.interface';
-
+const areEqual = (prevProps: any, nextProps: any) => {
+  if (prevProps.transactions.length !== nextProps.transactions.length) return false;
+  for (let i = 0; i < prevProps.transactions.length; i += 1) {
+    if (prevProps.transactions[i] !== nextProps.transactions[i]) return false;
+  }
+  return true;
+}
 const TransactionCardWrapper: React.FC<TransactionCardWrapperProps> = ({
   transactions,
   title,
   totalAmount
 }) => {
-  const transactionsList = transactions.map((transaction: Transaction) => {
+  const transactionsList = transactions.map(transaction => {
     const { _id: transactionId } = transaction;
-    return (<li key={transactionId.toString()}>
+    return (<li key={transactionId}>
       <TransactionCard transaction={transaction} />
     </li>)
   })
@@ -28,4 +33,4 @@ const TransactionCardWrapper: React.FC<TransactionCardWrapperProps> = ({
   )
 }
 
-export default TransactionCardWrapper;
+export default React.memo(TransactionCardWrapper, areEqual);

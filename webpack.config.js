d28b15b4598f051a249a69e2b3984dcc1d75967c
Ankit Tiwari <ankit90499@gmail.com>
2022-02-12 22:48:41 +0530

    fixed: webpack errors , finally done with custom webpack setup :)

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+module.exports = {
+  mode: mode,
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  ...(mode === 'development') && {
+    devtool: "cheap-module-source-map",
+    devServer: {
+      hot: true,
+      open: true
+    },
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '..', './build')
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      Constants: path.resolve(__dirname, 'src/Constants'),
+      helper: path.resolve(__dirname, 'src/helper'),
+      actions: path.resolve(__dirname, 'src/actions'),
+      reducers: path.resolve(__dirname, 'src/reducers'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      customHooks: path.resolve(__dirname, 'src/customHooks'),
+      'api-services': path.resolve(__dirname, 'src/api-services')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    })
+  ]
+};
--- webpack/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common');
-
-module.export = (envVars) => {
-  const { env } = envVars;
-  const envConfig = require(`./webpack/${env}.js`);
-  const config = merge(commonConfig, envConfig);
-  return config;
-}

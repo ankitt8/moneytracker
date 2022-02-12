c41bdb1bc3c56ebb7b9cbd4df7beb8bec087ea36
Ankit Tiwari <ankit90499@gmail.com>
2022-02-12 23:42:47 +0530

    feat: fixed build output directory path

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, '..', './build')
+    path: path.resolve(__dirname, './build')
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],

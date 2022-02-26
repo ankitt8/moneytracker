7597377c5e1e14dcf2f53026f7f8504bd638b3cb
Ankit Tiwari <ankit90499@gmail.com>
2022-02-26 21:46:01 +0530

    feat: fixed process not defined error (#130)
    Reason
    - process.env.PUBLIC_URL was being used in service worker file created by CRA
    - since PUBILC_URL was not defined
    - in webpack configuration
    
    TODO
    - currently inside build folder service worker isnot created

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-})
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.PUBLIC_URL': JSON.stringify('')
+    })
+  ]
+});

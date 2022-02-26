8d18eafb91042a17a09c87a700b221bf0e8a1887
Ankit Tiwari <ankit90499@gmail.com>
2022-02-26 23:59:58 +0530

    fix: public assets not found error (#131)
    - public/index.html file was used in HtmlWebpack plugin
    - so inside dist to same file was used and it was used as template to create index.html file
    - inside public index.html file images, icons, manifest.json files were correctly referenced since they were found in public folder
    - but all those images, icons, manifest.json files weren't part of build folder
    - used copy-webpack-plugin to copy the public folder contents into build during build process
    - hence added the plugin only in webpack.prod.js file
    - tried with multiple patterns since whenever i was trying with any pattern it created a seperate folder in build then a public inside it and so on
    - after some googling came up to this configuration which was working fine
    Links
    - https://stackoverflow.com/a/69081475/9871509
    - https://webpack.js.org/plugins/copy-webpack-plugin/#copy-in-new-directory

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
+const path = require('path');
 const { merge } = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -8,6 +10,19 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       'process.env.PUBLIC_URL': JSON.stringify('')
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          to({ context, absoluteFilename }) {
+            return `./${path.relative(context, absoluteFilename)}`;
+          },
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html']
+          }
+        }
+      ]
     })
   ]
 });

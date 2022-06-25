78115fd90d1e1a2f15fdf680baf3df61017e8d9d
Ankit Tiwari <ankit90499@gmail.com>
2022-06-25 23:23:08 +0530

    hotfix: respondWith returnign null

--- public/sw.js
+++ public/sw.js
@@ -29,27 +29,22 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   // stale while revalidate approach
   e.respondWith(
-    caches.open(staticAssetsCacheName).then((cache) => {
-      return cache.match(e.request).then((cachedResponse) => {
-        // console.log(cachedResponse ? cachedResponse : 'Its not in the cache');
-        if (e.request.destination === 'image') {
-          return cachedResponse;
-        }
-        if (e.request.destination.includes('api')) {
-          return fetch(e.request);
-        }
-        // if(!event)
-        const networkFetch = fetch(e.request).then((res) => {
-          if (res.status === 404) {
-            return new Response('opps');
-          }
-          caches.open(staticAssetsCacheName).then((cache) => {
-            cache.put(e.request, res.clone());
-          });
+    caches.match(e.request).then((cachedResponse) => {
+      // console.log(cachedResponse ? cachedResponse : 'Its not in the cache');
+      if (e.request.destination === 'image') {
+        return cachedResponse;
+      }
+      if (e.request.destination.includes('api')) {
+        return fetch(e.request);
+      }
+      // if(!event)
+      const networkFetch = fetch(e.request).then((res) => {
+        caches.open(staticAssetsCacheName).then((cache) => {
+          cache.put(e.request, res.clone());
         });
-        // console.log({ cachedResponse, networkFetch });
-        return cachedResponse || networkFetch;
       });
+      console.log({ cachedResponse, networkFetch });
+      return cachedResponse || networkFetch;
     })
   );
 });

d0ac83b0c8a1ceb61481147b5909b0b62895d7c1
Ankit <ankit90499@gmail.com>
2023-03-25 16:24:17 +0530

    feat: add bank account WIP

--- src/pages/BankAccountsPage/index.tsx
+++ src/pages/BankAccountsPage/index.tsx
@@ -1,12 +1,31 @@
 import styles from './styles.module.scss';
-
+import useAddData from 'customHooks/useAddData';
+import { addBankAccountDB } from '../../api-services/api.service';
 const BankAccountsPage = () => {
+  const addBankApiSuccessHandler = (res: any) => {
+    console.log(res);
+  };
+  const addBankApiErrorHandler = (e: string) => {
+    console.log(e);
+  };
+  const { addDataApiCall: addBankAccount, state } = useAddData(
+    addBankApiSuccessHandler,
+    addBankApiErrorHandler
+  );
+
   const addBankFormSubmitHandler = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
+    let bankAccountName = '';
     for (const [key, value] of formData) {
       console.log({ key, value });
+      if (key === '') {
+        bankAccountName = value;
+      }
     }
+    addBankAccount(() =>
+      addBankAccountDB('234', { name: bankAccountName, balance: 0 })
+    );
   };
   return (
     <>

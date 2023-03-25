d704228fe6fad0e60a1169bbfac28ca9d9ba6e2f
Ankit <ankit90499@gmail.com>
2023-03-25 14:42:53 +0530

    feat: add bank account

--- src/pages/BankAccountsPage/index.tsx
+++ src/pages/BankAccountsPage/index.tsx
@@ -1,7 +1,22 @@
 import styles from './styles.module.scss';
 
 const BankAccountsPage = () => {
-  return <div className={styles.bankAccounts}>BankAccounts</div>;
+  const handleAddBank = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    for (const [key, value] of formData) {
+      console.log({ key, value });
+    }
+  };
+  return (
+    <>
+      <div className={styles.bankAccounts}>BankAccounts</div>
+      <form onSubmit={handleAddBank}>
+        <input type={'text'} name={'accountName'} />
+        <button>Add bank account</button>
+      </form>
+    </>
+  );
 };
 
 export default BankAccountsPage;

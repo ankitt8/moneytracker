e73ed516863074515e476577236405b74c43ab4b
Ankit <ankit90499@gmail.com>
2023-03-26 14:20:22 +0530

    feat: using useApi hook to get transactions based no range

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -9,35 +9,19 @@ import { useEffect, useState } from 'react';
 import { FETCH_STATES } from 'reducers/DataReducer';
 import { months } from './constants';
 import styles from './style.module.scss';
+import useApi from '../../customHooks/useApi';
 const currentYear = new Date().getFullYear();
 interface IHistoryPageProps {
   userId: string;
 }
 function History({ userId }: IHistoryPageProps) {
-  /**
-   * give option to select date range
-   */
-  //   const [startDate, setStartDate] = useState(new Date('1 Jun 2022'));
-  //   const [endDate, setEndDate] = useState(new Date('30 Jun 2022'));
-  // const [monthSelected, setMonthSelected] = useState(() => {
-  //   return months[new Date().getMonth() - 1];
-  // });
-  // const [dateFilter, setDateFilter] = useState({ from: null, to: null });
-  // const monthSelectedIndex = months.findIndex(
-  //   (month) => month === monthSelected
-  // );
-  //   const [transactions, setTransactions] = useState([]);
-  const [refetchData, setRefetchData] = useState(false);
-  // useEffect(() => {
-  //   setRefetchData({});
-  // }, [monthSelected]);
+  const [transactions, setTransactions] = useState([]);
   const transactionTypes = ['credit', 'debit', 'borrowed'];
   const formValues = {
     startDate: null,
     endDate: null
   };
   const getTransactionsBasedOnRange = (e) => {
-    console.log('hi');
     e.preventDefault();
     const formData = new FormData(e.target);
     for (const [key, value] of formData) {
@@ -45,29 +29,15 @@ function History({ userId }: IHistoryPageProps) {
       formValues[key] = value;
     }
     console.log(formValues);
-    setRefetchData({});
+    getTransactionsApi(() => getTransactionsFromDB({ userId, ...formValues }));
   };
-  console.log(formValues);
-  const { fetchStatus, data } = useFetchData(
-    getTransactionsFromDB,
-    'Something Broke From Our End',
-    null,
-    refetchData,
-    {
-      userId: userId,
-      startDate: new Date(formValues.startDate).toDateString(),
-      endDate: new Date(formValues.endDate).toDateString()
-    }
+  const getTransactionsSuccessHandler = (transactions) => {
+    console.log({ transactions });
+    setTransactions(transactions);
+  };
+  const { addDataApiCall: getTransactionsApi, state } = useApi(
+    getTransactionsSuccessHandler
   );
-  // const handleChange = (e) => {
-  //   setMonthSelected(e.target.value || getCurrentMonth());
-  // };
-  // const handleDateChange = (e, type) => {
-  //   setDateFilter({
-  //     ...dateFilter,
-  //     [type]: e.target.value
-  //   });
-  // };
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -95,16 +65,19 @@ function History({ userId }: IHistoryPageProps) {
           <button>Go</button>
         </form>
       </div>
-      {fetchStatus.fetching === FETCH_STATES.PENDING && <LinearProgress />}
-      {data && <TransactionSummary transactions={data} />}
+      {state.loading && <LinearProgress />}
+      {transactions && <TransactionSummary transactions={transactions} />}
       {/* <Transactions
         month={monthSelectedIndex}
         transactions={data || []}
         fetching={fetchStatus.fetching}
         showTransactionsInAscendingOrder={true}
       /> */}
-      {data && (
-        <TransactionAnalysisPage userId={userId} transactionsProps={data} />
+      {transactions && (
+        <TransactionAnalysisPage
+          userId={userId}
+          transactionsProps={transactions}
+        />
       )}
     </div>
   );

551c3028dfbc20dddaa9399773982741f7354038
Ankit Tiwari <ankit90499@gmail.com>
2023-10-17 22:09:48 +0530

    feat: making project compatible with react 17

--- packages/common/src/reducers/index.ts
+++ packages/common/src/reducers/index.ts
@@ -1,6 +1,5 @@
 import { createStore, combineReducers } from 'redux';
 import { persistReducer, persistStore } from 'redux-persist';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import storage from 'redux-persist/lib/storage';
 import transactions from './transactions';
 import user from './user';
@@ -28,10 +27,10 @@ const rootReducer = combineReducers({
 });
 
 const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
-const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+// const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
 
 const storeCreator = () => {
-  const store = createStore(persistedReducer, composeEnhancers());
+  const store = createStore(persistedReducer);
   const persistor = persistStore(store);
   return { store, persistor };
 };

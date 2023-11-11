ce362a6186cfeb10ebf07500fcc79809a6b9482e
Ankit Tiwari <ankit90499@gmail.com>
2023-11-11 13:09:35 +0530

    feat: sperating logic from ui component

--- packages/common/src/pages/History/index.tsx
+++ packages/common/src/pages/History/index.tsx
@@ -34,217 +34,31 @@ import {
   getFlattenedCategories,
   removeDuplicateFromArray
 } from '../../utility';
+import { useHistoy } from './useHistoy';
 interface IHistoryPageProps {
   userId: string;
 }
-const FILTERS = {
-  groupByDate: false,
-  groupByPaymentType: false,
-  groupByCategory: false
-};
-const getInitialSelectedTransactionTypes = () => {
-  return TRANSACTION_TYPES.reduce((acc, curr) => {
-    return { ...acc, [curr]: true };
-  }, {});
-};
-const getFilterDisplayName = (filterKey) => {
-  if (filterKey === 'groupByDate') return 'Group by date';
-  if (filterKey === 'groupByPaymentType') return 'Group by payment type';
-  if (filterKey === 'groupByCategory') return 'Group by category';
-};
+
 enum DateFilters {
   currMonth = 'currMonth',
   currYear = 'currYear'
 }
 export default function History({ userId }: IHistoryPageProps) {
-  const categoriesStore = useSelector(
-    (store: ReduxStore) => store.transactions.categories
-  );
-  const bankAccounts = useSelector(
-    (store: ReduxStore) => store.user.bankAccounts
-  );
-  const creditCards = useSelector(
-    (store: ReduxStore) => store.user.creditCards
-  );
-  useFetchData(
-    getTransactionCategoriesFromDB,
-    GET_TRANSACTION_CATEGORIES_FAILURE_MSG,
-    getTransactionCategories,
-    null,
-    userId
-  );
-  useFetchData(
-    getPaymentInstrumentsFromDB,
-    GET_BANK_ACCOUNTS_FAILURE_MSG,
-    (paymentInstruments: string[]) =>
-      setUserPaymentInstrumentsAction(
-        PaymentInstruments.bankAccounts,
-        paymentInstruments
-      ),
-    null,
-    userId,
-    PaymentInstruments.bankAccounts
-  );
-  useFetchData(
-    getPaymentInstrumentsFromDB,
-    GET_BANK_ACCOUNTS_FAILURE_MSG,
-    (paymentInstruments: string[]) =>
-      setUserPaymentInstrumentsAction(
-        PaymentInstruments.creditCards,
-        paymentInstruments
-      ),
-    null,
-    userId,
-    PaymentInstruments.creditCards
-  );
-  const router = useRouter();
-  const transactionsFromApiRef = useRef([]);
-  const [transactionsToDisplay, setTransactionsToDisplay] = useState(
-    transactionsFromApiRef.current
-  );
-  const PAYMENT_INSTRUMENTS = [...bankAccounts, ...creditCards];
-  const [filters, setFilters] = useState<any>(() => ({
-    ...FILTERS,
-    groupByDate: true,
-    selectedTransactionTypes: getInitialSelectedTransactionTypes(),
-    selectedPaymentInstruments: PAYMENT_INSTRUMENTS,
-    categoriesSelected: getFlattenedCategories(categoriesStore),
-    startDate: constructStartDateOfYear(),
-    endDate: constructTodayDate()
-  }));
-  const selectedTransactionTypes = filters.selectedTransactionTypes ?? {};
-  const selectedPaymentInstruments = filters.selectedPaymentInstruments;
-  const selectedTransactionTypesArray: TRANSACTION_TYPE[] = useMemo(() => {
-    const temp = [];
-    for (const key in selectedTransactionTypes) {
-      if (selectedTransactionTypes[key]) temp.push(key);
-    }
-    return temp;
-  }, [selectedTransactionTypes]);
-  const categoriesToDisplay = removeDuplicateFromArray(
-    selectedTransactionTypesArray.reduce(
-      (acc: string[], curr) => [...acc, ...categoriesStore[curr]],
-      []
-    )
-  );
-  const categoriesSelected = removeDuplicateFromArray(
-    filters.categoriesSelected
-  );
-  const formValues = filters;
-  useEffect(() => {
-    const routerQueryParams = router.query;
-    if (Object.keys(routerQueryParams).length === 0) return;
-    const temp = { ...filters };
-    for (const [key, value] of Object.entries(routerQueryParams)) {
-      if (key && value && typeof value === 'string') {
-        temp[key] = JSON.parse(value);
-      }
-    }
-    setFilters((prevFilters) => {
-      return { ...prevFilters, ...temp };
-    });
-    const getTransactionsFilter = { ...temp, userId };
-    if (Object.keys(getTransactionsFilter).length > 0) {
-      getTransactionsApi(() =>
-        getTransactionsApiCallback({ ...getTransactionsFilter })
-      );
-    }
-  }, []);
-  useEffect(() => {
-    setTransactionsToDisplay(
-      getFilteredTransactions(transactionsFromApiRef.current, {
-        category: removeDuplicateFromArray(categoriesSelected)
-      })
-    );
-  }, [filters.categoriesSelected]);
-  const getTransactionsApiCallback = (getTransactionsFilter) => {
-    const updatedGetTransactionsFilter = { ...getTransactionsFilter };
-    selectedPaymentInstruments?.forEach((selectedPaymentInstrument) => {
-      if (bankAccounts?.includes(selectedPaymentInstrument)) {
-        updatedGetTransactionsFilter?.selectedBankAccounts?.push(
-          selectedPaymentInstrument
-        );
-      }
-      if (creditCards?.includes(selectedPaymentInstrument)) {
-        updatedGetTransactionsFilter?.selectedCreditCards?.push(
-          selectedPaymentInstrument
-        );
-      }
-      if (
-        updatedGetTransactionsFilter?.selectedBankAccounts?.length ===
-        bankAccounts.length
-      ) {
-        updatedGetTransactionsFilter.selectedBankAccounts = [];
-      }
-      if (
-        updatedGetTransactionsFilter?.selectedCreditCards?.length ===
-        bankAccounts.length
-      ) {
-        updatedGetTransactionsFilter.selectedCreditCards = [];
-      }
-    });
-    let newUrl = '/history?';
-    const getTransactionsFilterEntries = Object.entries(
-      updatedGetTransactionsFilter
-    );
-    for (let i = 0; i < getTransactionsFilterEntries.length; i++) {
-      const [key, value] = getTransactionsFilterEntries[i];
-      if (value) {
-        newUrl += `${key}=${JSON.stringify(value)}`;
-        if (i !== getTransactionsFilterEntries.length - 1) {
-          newUrl += '&';
-        }
-      }
-    }
-    window.history.pushState({}, '', `${newUrl}`);
-    return getTransactionsFromDB(updatedGetTransactionsFilter);
-  };
-  const transactionHistoryFormSubmitHandler = (e?: any) => {
-    e?.preventDefault();
-    const formData = new FormData(e.target);
-    for (const [key, value] of formData) {
-      formValues[key] = value;
-    }
-    const getTransactionsFilter = {
-      categoriesSelected: removeDuplicateFromArray(categoriesSelected),
-      transactionTypes: selectedTransactionTypesArray,
-      selectedBankAccounts: [],
-      selectedCreditCards: [],
-      startDate: formValues.startDate,
-      endDate: formValues.endDate,
-      userId
-    };
-
-    getTransactionsApi(async () => {
-      return getTransactionsApiCallback(getTransactionsFilter);
-    });
-  };
-  const getTransactionsSuccessHandler = (transactions) => {
-    transactionsFromApiRef.current = transactions;
-    setTransactionsToDisplay(transactions);
-  };
-  const { apiCall: getTransactionsApi, state } = useApi(
-    getTransactionsSuccessHandler
-  );
-  const handleDateFilterClick = (type: DateFilters) => {
-    let startDate = '';
-    if (type === DateFilters.currMonth) {
-      startDate = constructStartDateOfMonth();
-    } else {
-      startDate = constructStartDateOfYear();
-    }
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      startDate
-    }));
-    getTransactionsApi(() =>
-      getTransactionsApiCallback({
-        ...filters,
-        startDate,
-        userId
-      })
-    );
-  };
+  const {
+    transactionHistoryFormSubmitHandler,
+    filters,
+    setFilters,
+    handleDateFilterClick,
+    selectedTransactionTypes,
+    PAYMENT_INSTRUMENTS,
+    selectedPaymentInstruments,
+    categoriesToDisplay,
+    categoriesSelected,
+    state,
+    transactionsToDisplay,
+    FILTERS,
+    getFilterDisplayName
+  } = useHistoy({ userId });
   return (
     <div className={styles.container}>
       <form
@@ -461,13 +275,13 @@ export default function History({ userId }: IHistoryPageProps) {
         endDateParam={
           transactionsToDisplay?.length > 0
             ? new Date(
-                transactionsToDisplay[transactionsToDisplay.length - 1].date
-              ).toDateString()
+                transactionsToDisplay[transactionsToDisplay.length - 1]?.date
+              )?.toDateString()
             : ''
         }
         startDateParam={
           transactionsToDisplay?.length > 0
-            ? new Date(transactionsToDisplay[0].date).toDateString()
+            ? new Date(transactionsToDisplay[0]?.date)?.toDateString()
             : ''
         }
         isNoTransactionsDateVisible={true}

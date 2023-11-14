d2b804139277defa2ccf129c9f2016f734ace309
Ankit Tiwari <ankit90499@gmail.com>
2023-11-14 19:07:35 +0530

    Revert "Revert "Bundle check test categories page (#152)" (#153)"
    This reverts commit 0b6c96e129b694f10116cb771611d004db435e97.

--- packages/common/src/components/DisplayCategories/index.tsx
+++ packages/common/src/components/DisplayCategories/index.tsx
@@ -1,16 +1,13 @@
 import { DisplayCategoriesProps } from './interface';
 import styles from './styles.module.scss';
+import { motion } from 'framer-motion';
 import { NO_CATEGORIES_FOUND } from '../../Constants';
 import { Cross } from '../Icons/Cross';
 
 const DisplayCategories = ({
   categories,
-  handleDeleteCategory,
-  isLoaderVisible = false
+  handleDeleteCategory
 }: DisplayCategoriesProps) => {
-  if (isLoaderVisible) {
-    return <p className={styles.noData}>Loading categories...</p>;
-  }
   if (!categories || categories.length === 0) {
     return <p className={styles.noData}>!!{NO_CATEGORIES_FOUND}!!</p>;
   }
@@ -18,12 +15,18 @@ const DisplayCategories = ({
     <div className={styles.categoriesWrapper}>
       {categories.map((category) => {
         return (
-          <div key={category} className={styles.categoryWrapper}>
+          <motion.div
+            initial={{ opacity: 0, y: -5 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.2, delay: 0 }}
+            key={category}
+            className={styles.categoryWrapper}
+          >
             <div>{category}</div>
             <button onClick={() => handleDeleteCategory(category)}>
               <Cross />
             </button>
-          </div>
+          </motion.div>
         );
       })}
     </div>

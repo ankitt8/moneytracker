7921ff6bfa0ce3b6643588358c92d272486f4d09
Ankit Tiwari <ankit90499@gmail.com>
2023-11-19 12:10:53 +0530

    feat: reduced login page bundle size by 15kB after gzip (#165)

---
+++ packages/common/src/components/Icons/EyeClosed.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EyeClosed = () => {
+  return <p>&#x1F453;</p>;
+};
---
+++ packages/common/src/components/Icons/EyeOpen.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EyeOpen = () => {
+  return <p>&#x1F441;</p>;
+};
